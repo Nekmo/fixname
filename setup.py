36d1a154e286a91f3f876d7e33af31d186f8aeaa
Nekmo <contacto@nekmo.com>
2011-09-30 03:39:48 +0200

    Soporte para establecer logging, reporte de cuando no hay archivos, estadísticas con valores diferentes por letra

---
+++ setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+#
+# Nekmo Software 2011 ( contacto [at] nekmo.com ) - Licencia: GPLv3 
+# 
+# Envíe sus sugerencias y reportes a: https://bitbucket.org/Nekmo/namerepair
+#
+##
+
+from distutils.core import setup
+import sys
+
+VERSION = '1.0'
+DESCRIPTION = 'Reparar nombres de fichero incorrectos (con carácter "�"),'\
+              'para poder trabajar con ellos.'
+
+LONG_DESCRIPTION = """
+Este renombrador de archivos le permite, de manera sencilla, renombrar
+aquellos archivos que tienen caracteres extraños (del tipo "�") que impiden
+trabajar con ellos. 
+"""
+
+CLASSIFIERS = [
+    'Topic :: System :: Filesystems',
+    'Intended Audience :: End Users/Desktop',
+    'Intended Audience :: System Administrators',
+]
+
+packages = []
+
+scripts = ['namerepair']
+
+setup(
+    name = "namerepair",
+    version = VERSION,
+    description = DESCRIPTION,
+    long_description = LONG_DESCRIPTION,
+    author = 'Nekmo',
+    author_email = 'contacto [at] nekmo.com',
+    url = 'http://nekmo.com',
+    license = 'GPLv3',
+    platforms = ['any',],
+    packages = packages,
+    requires = ['argparse'],
+    scripts = scripts
+)
